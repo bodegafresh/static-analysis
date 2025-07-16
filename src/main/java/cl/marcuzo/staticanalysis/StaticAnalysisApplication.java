cb5553c168cfbf56377ec1be67a67afa28fc8967
Marco Cerda <marco.cerda@mercadolibre.cl>
2025-07-15 20:33:25 -0400

    test

--- src/main/java/cl/marcuzo/staticanalysis/StaticAnalysisApplication.java
+++ src/main/java/cl/marcuzo/staticanalysis/StaticAnalysisApplication.java
@@ -3,10 +3,14 @@ package cl.marcuzo.staticanalysis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootApplication
+@Log4j2
 public class StaticAnalysisApplication {
 
 	public static void main(String[] args) {
+		log.info("iniciando: {}", args.length);
 		SpringApplication.run(StaticAnalysisApplication.class, args);
 	}
 
